a10954618ef4cb71caad12bc94d8b9332b7cfc97
Oystein Graendsen <oysteigr@gmail.com>
2013-02-27 17:03:54 +0100

    made containers to group head and body. Started looking into new panels

--- window_test.py
+++ window_test.py
@@ -9,68 +9,97 @@ class MyButton(wx.Button):
 		font = wx.Font(20,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
 		self.SetFont(font)
 		
-class MyForm(wx.Frame):
+class SpeedFrame(wx.Frame):
 
 	def __init__(self):
-		wx.Frame.__init__(self, None, wx.ID_ANY, "Button Tutorial",size=(1024,768))
-		panel = wx.Panel(self, wx.ID_ANY)
-		panel.SetBackgroundColour('#ff8c02')
+		wx.Frame.__init__(self, None, wx.ID_ANY, "SpeedFrame",size=(1024,768))
+		panel1 = wx.Panel(self, wx.ID_ANY, size=(1024,168))
+		panel2 = wx.Panel(self, wx.ID_ANY, size=(1024,600))
+		
+		panel1.SetBackgroundColour('#ff8c02')
+		panel2.SetBackgroundColour('#000000')
 		#font = wx.Font(20,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
 		#panel.SetFont(font)
+		box = wx.BoxSizer(wx.VERTICAL)
+		box.Add(panel1, 168, flag=wx.ALIGN_TOP|wx.LEFT)
+		box.Add(panel2, 600, flag=wx.ALIGN_BOTTOM|wx.LEFT)
 		
-		button1 = MyButton(panel, id=wx.ID_ANY, label="SPEED",pos=(0,168))
-		button2 = MyButton(panel, id=wx.ID_ANY, label="DISTANCE",pos=(512,168))
-		button3 = MyButton(panel, id=wx.ID_ANY, label="ENERGY",pos=(0,368))
-		button4 = MyButton(panel, id=wx.ID_ANY, label="SOLAR PANEL",pos=(512,368))
-		button5 = MyButton(panel, id=wx.ID_ANY, label="GPS",pos=(0,568))
-		button6 = MyButton(panel, id=wx.ID_ANY, label="MUSIC",pos=(512,568))
+		self.SetSizer(box)
+
+		
+class MainFrame(wx.Frame):
+
+	def __init__(self):
+		wx.Frame.__init__(self, None, wx.ID_ANY, "MainFrame",size=(1024,768))
+		panel_top = wx.Panel(self, wx.ID_ANY, size=(1024,168))
+		panel_main = wx.Panel(self, wx.ID_ANY, size=(1024,600))
+		panel_speed = wx.Panel(self, wx.ID_ANY, size=(1024,600))
+		
+		panel_speed.Hide()
+		
+		panel_top.SetBackgroundColour('#ff8c02')
+		panel_main.SetBackgroundColour('#000000')
+		#font = wx.Font(20,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+		#panel.SetFont(font)
+		box = wx.BoxSizer(wx.VERTICAL)
+		box.Add(panel_top, 168, flag=wx.ALIGN_TOP|wx.LEFT)
+		box.Add(panel_main, 600, flag=wx.ALIGN_BOTTOM|wx.LEFT)
+		box.Add(panel_speed, 600, flag=wx.ALIGN_BOTTOM|wx.LEFT)
+		
+		self.SetSizer(box)
+		
+		button1 = MyButton(panel_main, id=wx.ID_ANY, label="SPEED",pos=(0,0))
+		button2 = MyButton(panel_main, id=wx.ID_ANY, label="DISTANCE",pos=(512,0))
+		button3 = MyButton(panel_main, id=wx.ID_ANY, label="ENERGY",pos=(0,200))
+		button4 = MyButton(panel_main, id=wx.ID_ANY, label="SOLAR PANEL",pos=(512,200))
+		button5 = MyButton(panel_main, id=wx.ID_ANY, label="GPS",pos=(0,400))
+		button6 = MyButton(panel_main, id=wx.ID_ANY, label="MUSIC",pos=(512,400))
 		
 		
-		button1.Bind(wx.EVT_BUTTON, self.onButton1)
+		button1.Bind(wx.EVT_BUTTON, self.onButton1, panel_main, panel_speed)
 		button2.Bind(wx.EVT_BUTTON, self.onButton2)
 		button3.Bind(wx.EVT_BUTTON, self.onButton3)
 		button4.Bind(wx.EVT_BUTTON, self.onButton4)
 		button5.Bind(wx.EVT_BUTTON, self.onButton5)
 		button6.Bind(wx.EVT_BUTTON, self.onButton6)
-
-		
+	
 		self.Refresh()
 
-    #----------------------------------------------------------------------
-	def onButton1(self, event):
-        	"""
-	        This method is fired when its corresponding button is pressed
-	        """
-	        print "Button1 pressed!"
+#----------------------------------------------------------------------
+	def onButton1(self, event, panel1, panel2):
+		"""
+		This method is fired when its corresponding button is pressed
+		"""
+		print "Button1 pressed!"
 	def onButton2(self, event):
-	        """
-	        This method is fired when its corresponding button is pressed
-	        """
-	        print "Button2 pressed!"
+		"""
+		This method is fired when its corresponding button is pressed
+		"""
+		print "Button2 pressed!"
 	def onButton3(self, event):
-	        """
-	        This method is fired when its corresponding button is pressed
-	        """
-	        print "Button3 pressed!"
+		"""
+		This method is fired when its corresponding button is pressed
+		"""
+		print "Button3 pressed!"
 	def onButton4(self, event):
-	        """
-	        This method is fired when its corresponding button is pressed
-	        """
-	        print "Button4 pressed!"
+		"""
+		This method is fired when its corresponding button is pressed
+		"""
+		print "Button4 pressed!"
 	def onButton5(self, event):
-	        """
-	        This method is fired when its corresponding button is pressed
-	        """
-	        print "Button5 pressed!"
+		"""
+		This method is fired when its corresponding button is pressed
+		"""
+		print "Button5 pressed!"
 	def onButton6(self, event):
-	        """
-	        This method is fired when its corresponding button is pressed
-	        """
-	        print "Button6 pressed!"    
+		"""
+		This method is fired when its corresponding button is pressed
+		"""
+		print "Button6 pressed!"    
 # Run the program
 if __name__ == "__main__":
 	app = wx.App(False)
-	frame = MyForm()
+	frame = MainFrame()
 	frame.Show()
 	frame.ShowFullScreen(True)
 	app.MainLoop()
